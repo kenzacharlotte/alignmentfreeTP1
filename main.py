93c721e69c815074beb55a42a7a852b061e4b28f
kenzacharlotte <108465390+kenzacharlotte@users.noreply.github.com>
2023-12-07 00:38:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
 
+def my_method(text,ia,ib,k):
+    """
+    Takes 2 sequences and returns the kmers present in each sequence.
+    
+    Parameters : 
+    text : (array) each 
+    ia,ib : (int) the index of the sequences to compare 
+    k : (int) length of the kmers
+    
+    """
+    Dkmer = {} 
+    U = 0 #Kmer union
+    I = 0 #Kmer intersection
+    for kmer in stream_kmers(text[ia],k): #génère les kmer de la séquence A et les stocke dans un dictionnaire
+        if kmer in Dkmer:
+            Dkmer[kmer]+=1
+        else:
+            Dkmer[kmer]=1
+        U+=1
+    
+    for kmer2 in stream_kmers(text[ib],k):
+        #génère les kmer de la séquence B et les compares avec ceux déjà  #présents dans le dictionnaire
+        if kmer2 in Dkmer.keys() :
+            I+=1
+            Dkmer[kmer2]-=1
+            if Dkmer[kmer2]==0:
+                del Dkmer[kmer2]
+        else :
+            #Dkmer[kmer]+=1
+            U+=1
+        return U,I
 
 
-def similarity(A, inter, B):
-    # --- To complete ---
-    pass
+def similarity(A,B):
+    """
+    Calcule la similarité entre les deux séquences
+    """
+    intersection_size = len(A.intersection(B))    
+    return intersection_size/len(A),intersection_size/len(B)
 
 
-def jaccard(A, inter, B):
-    # --- To complete ---
-    pass
-
+def jaccard(A, B):
+    """
+    Calcule la distance de jaccard entre ecs 
+    """
+    intersection_size = len(A.intersection(B))
+    union_size = len(A.union(B))
+    
+    return intersection_size/union_size
 
 
 if __name__ == "__main__":
